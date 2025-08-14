aeb520da6cc56e39c5905fb5d19a2d638fc05091
domthecoder-dev <dominic3lliott@gmail.com>
2025-08-14 18:00:32 +0200

    Update invoicerequest-form.jsx

--- src/compontents/invoicerequest-form.jsx
+++ src/compontents/invoicerequest-form.jsx
@@ -44,7 +44,7 @@ export default function InvoiceRequestForm() {
     if (data.success) {
       setCustomerName('');
       setCustomerEmail('');
-      CartItems([]);
+      setCartItems([]);
     }
 
   } catch (error) {
